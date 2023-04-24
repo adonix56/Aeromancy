0398017ef0e0cb9efc84d23c720066925c1e820e
Justin Salanga <adonix56@gmail.com>
2023-04-24 07:48:37 -0700

    Connected Ending to the main menu

--- Assets/Ending.cs
+++ Assets/Ending.cs
@@ -2,10 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
+using UnityEngine.SceneManagement;
 
 public class Ending : MonoBehaviour
 {
     [SerializeField] private PlayableDirector cutscene;
+    private bool isPlaying;
+
+
+    private void Update() {
+        if (isPlaying) {
+            if (cutscene.state == PlayState.Paused)
+                SceneManager.LoadScene(0);
+        }
+    }
+
     private void OnTriggerEnter(Collider other) {
         CharacterManager.Instance.SetPlayable(false);
         cutscene.Play();
